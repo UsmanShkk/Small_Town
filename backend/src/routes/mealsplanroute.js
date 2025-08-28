bccf9f5b53d8d09e9e2f976b1a16b86177e9bcdc
Usmanshkk <shkmusman455@gmail.com>
2025-08-28 18:31:08 +0500

    Initial Commit , Set up email automation by Usman

--- backend/src/routes/mealsplanroute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { saveMealPlan, getMealPlan } = require("../controllers/parsedata");
-const router = express.Router();
-
-router.post("/save", saveMealPlan);
-router.get("/:userId", getMealPlan);
-
-module.exports = router;
